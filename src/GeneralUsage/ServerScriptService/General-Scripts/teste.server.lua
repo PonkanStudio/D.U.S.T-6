5d255cf1228a601a007ea99ab41adef1906e57b7
PonkanStudio <studio.ponkan@gmail.com>
2024-06-17 15:17:07 -0300

    teste

--- src/GeneralUsage/ServerScriptService/General-Scripts/teste.server.lua
+++ src/GeneralUsage/ServerScriptService/General-Scripts/teste.server.lua
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
 print("Olá jumbo!")
 
 
-local pao = Instance.new("Part")
-pao.Parent = game.Workspace
+local pao2 = Instance.new("Part")
+pao2.Parent = game.Workspace
 
 -- Teste
 -- comento
-=======
+
 print("oi piranha, isso é um teste")
-local pao = 1
+
 local fao = 2
 local vao = 3
->>>>>>> a7c8ab6593950e48307f48b7b568abf3ebc4e31f
+
+
