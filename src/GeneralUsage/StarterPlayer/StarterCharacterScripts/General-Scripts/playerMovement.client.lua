ab36f26cddefd8b1b4496dcc47c16a06d7ba2e92
PonkanStudio <studio.ponkan@gmail.com>
2024-06-20 22:45:52 -0300

    Updates on custom movement system

--- src/GeneralUsage/StarterPlayer/StarterCharacterScripts/General-Scripts/playerMovement.client.lua
+++ src/GeneralUsage/StarterPlayer/StarterCharacterScripts/General-Scripts/playerMovement.client.lua
@@ -10,33 +10,77 @@ local Humanoid = Character:WaitForChild("Humanoid") -- Wait for the Humanoid to 
 
 local lastTick = tick()
 
+local runSpeed = 24
+local crouchSpeed = 6
+local crawlSpeed = 3
 
+
+local isRunning = false -- Control variable to check if player is Running
+local isCrouching = false -- Control variable to check if player is Crouching
+local isCrawling = false -- Control variable to check if player is Crawling
 -- </ System >
 
+-- Movement System
+
+local function stopCustomMovement() -- A function to stop all the custom movements
+    Humanoid.WalkSpeed = SP.CharacterWalkSpeed -- Sets the character walkspeed to the game default
+
+    isRunning = false
+    isCrouching = false
+    isCrawling = false
+end
     -- Run System
-local function startRunning()
-    Humanoid.WalkSpeed = 50 -- Sets the Cahracter walkspeed to 50 
+local function startRunning() 
+    stopCustomMovement() -- Stop all custom movement player was doing
+    Humanoid.WalkSpeed = runSpeed  -- Sets the Character walkspeed to 50 
+    isRunning =  true -- Sets the Control variable to true after reseting it in stopCustomMovement function
 end
 
-local function stopRunning()
-    Humanoid.WalkSpeed = SP.CharacterWalkSpeed -- Sets the Character walkspeed to the game default walking speed
+    -- Crouch System
+local function startCrouching()
+    stopCustomMovement() -- Stop all custom movement player was doing
+    Humanoid.WalkSpeed = crouchSpeed -- Sets the Character walkspeed to defined crouch speed
+    isCrouching =  true -- Sets the Control variable to true after reseting it in stopCustomMovement function
+end
+
+     -- Crawl System
+
+local function startCrawling()
+    stopCustomMovement() -- Stop all custom movement player was doing
+    Humanoid.WalkSpeed = crawlSpeed -- Sets the Character walkspeed to defined crawl speed
+    isCrawling =  true -- Sets the Control variable to true after reseting it in stopCustomMovement function
 end
 
 UIS.InputBegan:Connect(function(input,GPE) -- Get all inputs 
     if GPE then return end -- Return if is on Game Processed Event, it means they're typing
 
-    if input.KeyCode == Enum.KeyCode.W then --  Checking if the pressed key was W
+    if input.KeyCode == Enum.KeyCode.W then --  Checking if the pressed key was W to start running
         if tick() - lastTick <= 0.5 then -- Checking if the interval of time between this click of W and the last one was less than 0.5 seconds
             startRunning() -- Calls the function to start running
         end
 
-        lastTick = tick() -- Reseting the last tick
+    lastTick = tick() -- Reseting the last tick
+    elseif input.KeyCode == Enum.KeyCode.C then --  Checking if the pressed key was C to start crouching
+        if isCrouching then
+            stopCustomMovement() -- Calls the fuction to stop custom movement
+            isCrouching = false
+        else
+            startCrouching() -- Calls the function to start crouching
+        end
+    elseif input.KeyCode == Enum.KeyCode.Z then --  Checking if the pressed key was Z to start crawling
+        if isCrawling then
+            stopCustomMovement() -- Calls the fuction to stop custom movement
+            isCrawling = false
+        else
+            startCrawling() -- Calls the function to start crawling   
+        end
     end
 end)
 
 
 UIS.InputEnded:Connect(function(input,GPE)
-    if input.KeyCode == Enum.KeyCode.W then
-        stopRunning() -- Calls the fuction to stop running
+    if input.KeyCode == Enum.KeyCode.W and isRunning and not isCrouching and not isCrawling then
+        stopCustomMovement() -- Calls the fuction to stop custom movement
+        isRunning = false
     end
 end)
