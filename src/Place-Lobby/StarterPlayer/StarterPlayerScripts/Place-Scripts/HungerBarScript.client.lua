053c8749cddf9b3e9534a23545064004c35b06b5
gusparck <gustavoaraujo.jesus@gmail.com>
2024-06-27 14:38:29 -0300

    BarrasCerto

--- src/Place-Lobby/StarterPlayer/StarterPlayerScripts/Place-Scripts/HungerBarScript.client.lua
+++ src/Place-Lobby/StarterPlayer/StarterPlayerScripts/Place-Scripts/HungerBarScript.client.lua
@@ -1,12 +1,11 @@
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
-local GUI = Player.PlayerGui:WaitForChild("HungerBar")
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local Humanoid = Character.Humanoid:WaitForChild("Humanoid")
+local GUI = Player.PlayerGui:WaitForChild("StatsBar")
 
-while wait(0.1) do
-    if script.Parent.Size.X.Scale <= 0 then
-        script.Parent.Parent.Parent.Parent.Character.Humanoid.Health -= 1
-        script.Parent.Size = UDim2.new(0.201, 0, 0.052, 0)
-    else
-        script.Parent.Size = UDim2.new(0.002, 0, 0, 0)
-    end
-end
+local HungerBar = GUI.Hunger_Stroke.Bar
+local ThirstBar = GUI.Thirst_Stroke.Bar
+local HealthBar = GUI.Health_Stroke.Bar
+
+HealthBar = Humanoid
